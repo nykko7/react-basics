4310db6c620a0a2ff68cd2f64c78c3d92495c43e
Nicolas P <n.perezbus@gmail.com>
2021-03-17 23:46:08 -0300

    24: styled components

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import HooksPersonalizados from './components/HooksPersonalizados';
 import Referencias from './components/Referencias';
 import Formularios from './components/Formularios';
 import Estilos from './components/Estilos';
+import ComponentesEstilizados from './components/ComponentesEstilizados';
 
 function App() {
 	return (
@@ -80,6 +81,8 @@ function App() {
 					<Formularios />
 					<hr />
 					<Estilos />
+					<hr />
+					<ComponentesEstilizados />
 					<br />
 					<br />
 					<br />
